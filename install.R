ba6fca7ba97f7ee09eaff878518641ea54a002f7
Yahor Mazko <yahor.mazko@soft-fx.lv>
2017-11-21 13:46:11 +0300

    Add installation

--- install.R
+++ install.R
@@ -1,9 +1,3 @@
-# rEmailSender
-
-R package for sending emails
-# How to install/update it?
-```
 if(!require(devtools)) {install.packages("devtools"); library(devtools)}	
 if(require(REmailSender)) {detach("package:REmailSender", unload=TRUE); remove.packages("REmailSender")}
-install_github("SoftFx/email",subdir = "R/REmailSender")	
-```
+install_github("SoftFx/email",subdir = "R/REmailSender")
